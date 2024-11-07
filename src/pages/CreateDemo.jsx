30d2cd6a60b26056801da19ef7455ece28f48da3
adarsh-pepsales <adarsh.t@pepsales.ai>
2024-11-08 01:52:48 +0530

    refactor : space adjustment

--- src/pages/CreateDemo.jsx
+++ src/pages/CreateDemo.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
-
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const CreateDemo = () => {
-
   return (
-      <main className="w-screen h-screen flex flex-col bg-slate-600">
+    <main className="w-screen h-screen flex flex-col bg-slate-600">
 
-        <Header/>
+      <Header />
 
-      <section className="w-full h-full flex-1 bg-green-300">
+      <section className="w-full h-full flex-1 ">
         <h1 className="text-center text-white">Main Content</h1>
       </section>
 
-       <Footer/>
-      </main>
-  )
-}
+      <Footer />
+    </main>
+  );
+};
 
-export default CreateDemo
+export default CreateDemo;
